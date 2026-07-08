30c52969c97f8d98a1956f5ff20070f5936be5e5
Sanjana02 <sanjanagschintu@gmail.com>
2026-07-08 16:11:22 +0530

    java code

--- door.java
+++ door.java
@@ -1,5 +1,8 @@
-class Door{
-    public static void main(String[] args){
+class Door {
+    public static void main(String[] args) {
         System.out.println("collaborating the git repo");
+        System.out.println("Adding new changes to the project");
+        System.out.println("Pushing updates to GitHub");
+        System.out.println("Code collaboration is successful");
     }
 }
